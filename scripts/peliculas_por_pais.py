3f97c83901e026095bff9c40a161645158ce6d6f
Ramón <ramonarj@ucm.es>
2021-01-13 15:19:59 +0100

    Ajustes para la paralelizacion
    Cambiadas las tareas (de 200 a 4) y los cores usados (de 1 a 4). El tiempo de ejecución mejora de 14 segundos a 10 segundos. 
    Faltaría detectar las características de los ejecutores en los que corre el programa

--- scripts/peliculas_por_pais.py
+++ scripts/peliculas_por_pais.py
@@ -8,8 +8,14 @@
 hechas por cada pais '''
 
 ## IMPORTS ##
+# Python
+import time
+start_time = time.time()
+import string
+import sys
+
 # Spark
-from pyspark import SparkConf, SparkContext
+from pyspark import SparkConf, SparkContext, SQLContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, split, sum, mean, ceil, collect_list, asc, desc
 
@@ -18,20 +24,29 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-# Python
-import string
-import sys
 
 ## CONSTANTES ##
 VISIBLE_COUNTRIES = 10 # Solo queremos mostrar 10 a la vez
 
 
-## PROCESAMIENTO DEL DATASET ##
-# Inicializamos Spark
-conf = SparkConf().setMaster('local').setAppName('MovieTrends')
+## INICILIAZACION DE SPARK ##
+''' BENCHMARK: para obtener tiempos optimos 
+- Numero de tareas = coincide con el num. de particiones del dataframe.
+Por defecto se crean 200, pero nosotros usamos una heuristica de [num. ejecutores * num.cores de cada ejecutor] 
+- Numero de ejecutores = 1 si se lanza en local, tantos como nodos si se trata de un cluster 
+- Numero de hilos/ejecutor = usaremos tantos cores como tenga el ejecutor (local[*])
+'''
+conf = SparkConf().setMaster('local[*]').setAppName('peliculas_por_pais')
 sc = SparkContext(conf = conf)
 spark = SparkSession(sc)
+sqlContext = SQLContext(sc)
 
+# Estas lineas para obtener el numero de ejecutores y cores no funcionan como deberian
+#NUM_EXECUTORS = conf.get('spark.executor.instances')
+#NUM_CORES = conf.get('spark.executor.cores')
+sqlContext.setConf("spark.sql.shuffle.partitions", "4")
+
+## PROCESAMIENTO DE LOS DATOS ##
 # Lectura del archivo csv: con la opcion "header" hacemos que la primera fila haga de cabecera
 DFVar = spark.read.option("header", "true").csv("./datasets/IMDb_movies.csv")
 
@@ -55,18 +70,20 @@ DFVar = DFVar.sort(desc("count"))
 # Preprocesado
 num_countries = DFVar.count()
 DFVar = DFVar.limit(VISIBLE_COUNTRIES - 1)
-DFVar.show()
 
 # Metadatos
 labels = DFVar.select("mainCountry").rdd.flatMap(lambda x: x).collect()
 sizes = DFVar.select("count").rdd.flatMap(lambda x: x).collect()
 explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0)
 
-# Se dibuja la figura
+# Se dibuja el grafico de tarta
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', 
 shadow=True, startangle=90)
-ax1.axis('equal') # circulo
+ax1.axis('equal') # para que sea un circulo
 
-plt.show()
+# La guardamos en el sistema de ficheros
 plt.savefig('./results/peliculas_por_pais.png')
+
+# Debug del tiempo, para el benchmarking
+print("--- %s seconds ---" % (time.time() - start_time))
