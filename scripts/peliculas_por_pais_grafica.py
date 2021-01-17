5d457454a127a12b4d69b82fc104f1920bb620ef
Ramón <ramonarj@ucm.es>
2021-01-17 18:12:07 +0100

    Actualizada grafica de peliculas por pais

--- scripts/peliculas_por_pais_grafica.py
+++ scripts/peliculas_por_pais_grafica.py
@@ -19,6 +19,7 @@ from pyspark import SparkConf, SparkContext, SQLContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, split, sum, mean, ceil, collect_list, asc, desc
 
+
 # Graficos
 import matplotlib
 matplotlib.use('Agg')
@@ -41,14 +42,12 @@ sc = SparkContext(conf = conf)
 spark = SparkSession(sc)
 sqlContext = SQLContext(sc)
 
-# Estas lineas para obtener el numero de ejecutores y cores no funcionan como deberian
-#NUM_EXECUTORS = conf.get('spark.executor.instances')
-#NUM_CORES = conf.get('spark.executor.cores')
+# Particiones de los datos: tantas como cores tengamos
 sqlContext.setConf("spark.sql.shuffle.partitions", "4")
 
 ## PROCESAMIENTO DE LOS DATOS ##
 # Lectura del archivo csv: con la opcion "header" hacemos que la primera fila haga de cabecera
-DFVar = spark.read.option("header", "true").csv("./datasets/IMDb_movies.csv")
+DFVar = spark.read.option("header", "true").csv("../datasets/IMDb_movies.csv")
 
 # Especificamos que columnas queremos usar para este caso...
 colNames = DFVar.schema.names 
@@ -59,6 +58,7 @@ droppedCols = set(colNames).symmetric_difference(set(wantedCols))
 DFVar = DFVar.drop(*droppedCols)
 
 # Nos quedamos solo con el pais principal de la pelicula
+num_movies = DFVar.count()
 DFVar = DFVar.withColumn("mainCountry", split(col("country"), "\\,").getItem(0))
 DFVar = DFVar.drop("country")
 
@@ -71,19 +71,28 @@ DFVar = DFVar.sort(desc("count"))
 num_countries = DFVar.count()
 DFVar = DFVar.limit(VISIBLE_COUNTRIES - 1)
 
+# Le metemos una fila al DF
+maincountries_movies = DFVar.agg(sum("count")).select("sum(count)").rdd.flatMap(lambda x: x).collect()[0]
+otros_lista = [["Others", num_movies - maincountries_movies]]
+otros_DF = spark.createDataFrame(otros_lista)
+DFVar = DFVar.union(otros_DF)
+DFVar.show()
+
 # Metadatos
 labels = DFVar.select("mainCountry").rdd.flatMap(lambda x: x).collect()
 sizes = DFVar.select("count").rdd.flatMap(lambda x: x).collect()
-explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0)
+explode = [0] * VISIBLE_COUNTRIES
+explode[0] = 0.1
 
 # Se dibuja el grafico de tarta
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', 
-shadow=True, startangle=90)
+ax1.pie(sizes, explode=explode, autopct='%1.1f%%', 
+pctdistance=1.1, shadow=True, startangle=90)
 ax1.axis('equal') # para que sea un circulo
+plt.legend(labels, loc = "upper left")
 
 # La guardamos en el sistema de ficheros
-plt.savefig('./results/peliculas_por_pais.png')
+plt.savefig('../results/peliculas_por_pais.png')
 
 # Debug del tiempo, para el benchmarking
 print("--- %s seconds ---" % (time.time() - start_time))
